401db3ae97ed7d7e504e2741693b141b48b93932
LeeKyoungMin6938 <ksan3940@naver.com>
2018-06-12 15:16:50 +0900

    마이페이지 수정중_이경민

--- src/app/user/user-main/user-main.component.ts
+++ src/app/user/user-main/user-main.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import {User} from '../../shared/models/user';
+import {Order} from '../../shared/models/order';
+import {OrderDetail} from '../../shared/models/orderDetail';
+import { AuthService } from "../../shared/services/auth.service";
+import { ProductService } from "../../shared/services/product.service";
 
 @Component({
   selector: 'app-user-main',
@@ -7,9 +12,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserMainComponent implements OnInit {
 
-  constructor() { }
+  orderLists : Order[]=[];
+  loggedInUser:User;
+
+  OrderCancle=0; // 주문취소건수
+  OrderCommit=0; // 주문완료건수
+  AllOrders=0; // 모든주문건수
+  constructor(
+    private productService:ProductService,
+    private authService:AuthService
+    // private loggedInUser:User
+  ) {   }
 
   ngOnInit() {
+    this.loggedInUser = this.authService.getLoggedInUser();
+    this.OnlyGetOrderList(this.loggedInUser.uid);
   }
 
+  OnlyGetOrderList(uid:string){
+    //getOrderList함수 = uid를 받아서 tm_orderlist (arraylist객체로)반환
+    this.productService.getOrderList(this.loggedInUser.uid).subscribe((data:Order[])=>{
+      this.orderLists = data;
+      for(let i=0;i<this.orderLists.length;i++){
+        this.AllOrders++;
+        if(this.orderLists[i].ostatus=='N'){
+          this.OrderCancle++;
+        }if(this.orderLists[i].ostatus=='Y'){
+          this.OrderCommit++;
+        }
+      }
+  });
+  }
 }
